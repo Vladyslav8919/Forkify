4667ba6c4fc6caa46d68d6be2b3149bedfa66526
Vladyslav8919 <vladyslav.pshevlotskyi@gmail.com>
2022-12-10 11:50:42 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c47b9225-2b6e-4357-8107-da84173f8c46';
+export const MODAL_CLOSE_SEC = 2.5;
